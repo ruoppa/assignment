4edc2805c549e966c0704ee87f4ba7f4df0b3a79
ruoppa <lassi.ruoppa@aalto.fi>
2022-01-27 23:43:37 +0200

    Add 4 new user types

--- user.py
+++ user.py
@@ -1,8 +1,10 @@
 import random
+import math
 from abc import ABCMeta, abstractmethod
 from datetime import datetime
 from typing import List, Optional
 from uuid import uuid4
+from black import out
 
 import names
 
@@ -17,12 +19,21 @@ class User(metaclass=ABCMeta):
     DummyUser is an example of how to do this.
     """
 
-    def __init__(self, type: str = "Base") -> None:
+    
+    # Added parameters success, miss and fail, which represent the
+    # probability of each of the three possible outcomes of a simulation. 
+    # Each user type has their own initial values for these parameters.
+    # Each parameter has accuracy of 3 decimals and their sum is always 1.
+    def __init__(self, success: float, miss: float, fail: float, type: str = "Base") -> None:
         """Init the object."""
         self.id: str = uuid4().hex
         self.type: str = type
         self.name: str = names.get_first_name()
         self.history: List[Optional[SimulationResult]] = []
+        self.success: float = success
+        self.miss: float = miss
+        self.fail: float = fail
+        
 
     @abstractmethod
     def _get_simulation_outcome() -> str:
@@ -31,6 +42,20 @@ class User(metaclass=ABCMeta):
         It should always return one of the possible SIMULATION_OUTCOMES
         """
         pass
+
+    # Most User classes determine the outcome the same way, i.e. 
+    # randomly, with each outcome having a probability given by
+    # the parameter of the same name. _get_simulation_outcome()
+    # uses this function to determine the outcome, then acts
+    # according to the logic of the class based on the outcome
+    def _outcome_helper(self) -> str:
+        c = round(random.uniform(0, 1), 3)
+        if c < self.success:
+            return "SUCCESS"
+        elif c < self.success + self.fail:
+            return "FAIL"
+        else:
+            return "MISS"
 
     def complete_simulation(self, timestamp: datetime) -> None:
         """Complete a simulation and store it in the user's history."""
@@ -57,27 +82,121 @@ class User(metaclass=ABCMeta):
     def __repr__(self) -> str:
         """Update the representation of a class object."""
         return (
-            f"User(id={self.id}, name={self.name}, type={self.type} "
+            f"User(id={self.id}, name={self.name}, type={self.type}, "
+            f"success={self.success}, miss={self.miss}, fail={self.fail}, "
             f"simulations_completed={self.simulations_completed})"
         )
 
-
-# TODO(Task 1): Implement your own user classes.
-# All classes should be inherited from the above User class.
-# See the DummyUser class below user for an example.
-class DummyUser(User):
-    """Dummy user class."""
+# Represents an user, who is disinterested and does not want to learn,
+# because they deem it useless. As such, the outcome of a simulation
+# will almost always be a miss with a small  chance of failing and initial
+# success probability of 0. Failing will momentarily increase the users
+# interest and success rate. Fail rate remains constant
+class UnmotivatedUser(User):
 
     def __init__(self) -> None:
-        """Init the object."""
-        super(DummyUser, self).__init__(type="Dummy")
+        # Random initial fail rate between 5-15% from uniform distribution
+        init_fail = round(random.uniform(0.05, 0.15), 3)
+        init_miss = 1 - init_fail
+        super(UnmotivatedUser, self).__init__(success = 0, miss = init_miss, fail = init_fail, type = "Unmotivated")
 
     def _get_simulation_outcome(self) -> str:
-        """
-        Implement a dummy simulation completion logic.
+        outcome = self._outcome_helper()
+        # If outcome is fail, success rate rises to ~0.2 * miss rate
+        if outcome == "FAIL":
+            self.success = round(self.miss * 0.2, 3)
+            self.miss = 1 - self.success - self.fail
+        # Success rate decays fast linearly with each success
+        elif outcome == "SUCCESS":
+            self.success = max(self.success - 0.04, 0)
+            self.miss = 1 - self.success - self.fail
+        return outcome
 
-        Please write your own classes and make the logic smarter! :)
-        """
-        # In your solution, tweak this logic to mimick your chosen user types instead
-        # of picking a random simulation outcome
-        return random.choice(SIMULATION_OUTCOMES)
+# Inactive user that e.g. is no longer with the organization, but
+# has not been removed from the database for some reason. Miss
+# rate of 100%. (Just some simulated noise to the data)
+class InactiveUser(User):
+
+    def __init__(self) -> None:
+        super(InactiveUser, self).__init__(success = 0, miss = 1, fail = 0, type = "Inactive")
+
+    def _get_simulation_outcome(self) -> str:
+        # Outcome is always a miss
+        return "MISS"
+
+# Represents the average user who is willing to improve and learn.
+# Starts with some somewhat randomized numbers for fail, miss and success.
+# User will "learn" from each miss and fail, which decreases the probability
+# of both, with the latter having a stronger effect than the former. Effect of
+# learning slows down as the success rate grows larger.
+class LearningUser(User):
+
+    def __init__(self) -> None:
+        # Amount of learning done by the user
+        self.knowledge = 0
+
+        init_miss = 2
+        # Determine initial miss rate with the help of a normal
+        # distribution, with expected value 0.6, standard deviation 0.15
+        while init_miss > 0.8 or init_miss < 0.1:
+            init_miss = round(random.gauss(0.6, 0.15), 3)
+        
+        # Initial fail rate between 5-20%
+        init_fail = round(random.uniform(0.05, 0.20), 3)
+        # Rest are successes
+        init_success = 1 - init_miss - init_fail
+        super(LearningUser, self).__init__(sucess = init_success, miss = init_miss, fail = init_fail, type = "Learning")
+
+    def _get_simulation_outcome(self) -> str:
+        # Possible amount of learning depends on the amount of learning
+        # so far
+        outcome = self._outcome_helper()
+        # If outcome is fail or miss, learning happens
+        if outcome == "FAIL" or outcome == "MISS":
+            # If outcome is miss, less learning
+            multiplier = 0.5 if outcome == "FAIL" else 0.3
+            learning = multiplier * (math.exp(-3 - self.knowledge) + 0.005)
+            self.miss = round(max(self.miss - learning, 0), 3)
+            self.fail = round(max(self.fail - learning, 0), 3)
+            self.success = 1 - self.miss - self.fail
+            # Knowledge increases with each learning experience
+            self.knowledge += 1
+
+        return outcome
+
+# Tech-savvy user, with very low fail rate and an initial miss rate
+# between 5-30%, to account for more and less interested tech-savvy
+# users. Failing will increase the success rate momentarily.
+class SavvyUser(User):
+
+    def __init__(self) -> None:
+        init_miss = round(random.uniform(0.05, 0.30), 3)
+        # Fail rate between 0 and 1/6 of miss rate
+        init_fail = round(random.uniform(0, init_miss / 6), 3)
+        init_success = 1 - init_fail - init_miss
+        # Parameter to store normal rates
+        self.init_success = init_success
+        self.init_fail = init_fail
+        self.init_miss = init_miss
+        super(SavvyUser, self).__init__(successs = init_success, miss = init_miss, fail = init_fail, type = "Savvy")
+
+    def _get_simulation_outcome(self) -> str:
+        outcome = self._outcome_helper()
+        # If outcome is a fail, success rises, miss and fail rates decrease
+        # momentarily
+        if outcome == "FAIL":
+            self.success += round((self.miss / 2 + self.fail / 2), 3)
+            self.miss = round(self.miss / 2, 3)
+            self.fail = 1 - self.success - self.miss
+        # Raised success rate decays back to normal linearly
+        elif self.init_success < self.success:
+            self.success = max(self.success - 0.02, self.init_success)
+            # If success rate is back to normal, restore miss and fail rates
+            if self.success == self.init_success:
+                self.miss = self.init_miss
+                self.fail = self.init_fail
+            else:
+                self.miss += 0.01
+                self.fail = 1 - self.miss - self.success
+
+        return outcome
